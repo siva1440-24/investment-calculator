d803097cf5e47a4cb8da02758badd56e93096221
siva1440-24 <sivamurikinati@gmail.com>
2024-09-30 19:13:17 +0530

    Add files via upload

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  // @Input()
+  // results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+  results = input<
+  {
+    year: number ,
+    interest : number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number ,
+    totalAmountInvested : number,
+
+  }[]>();
+
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../interface-input.model';
+import { signal } from '@angular/core';
+import { output } from '@angular/core';
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  // @Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
